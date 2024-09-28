93e44d7fa26e3c016f9df27a8ed868b5292b0603
Mohamed Farouk <mohamedfarouk1994@gmail.com>
2024-09-28 12:42:33 +0300

    optimizing and fixing the scraper

--- curiousscraper.py
+++ curiousscraper.py
@@ -2,113 +2,113 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 import time
 from selenium.common.exceptions import TimeoutException
 import pandas as pd
+from tqdm import tqdm
 
 
-# Initialize Selenium WebDriver (Chrome example)
-chrome_options = Options()
-# chrome_options.add_argument("--disable-blink-features=AutomationControlled")
-chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
-chrome_options.add_experimental_option('useAutomationExtension', False)
-chrome_options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36")
-driver = webdriver.Chrome(chrome_options)  # Make sure ChromeDriver is in your PATH or specify the location
-driver.get("https://curiouscat.live/mohfarouk94")  # Replace with the actual URL
-# action = ActionChains(driver)
-
-# Let the page load (adjust the sleep time as necessary)
-try:
-    myElem = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.CLASS_NAME, 'css-175oi2r')))
-except TimeoutException:
-    print("Loading took too much time!")
-    driver.quit()
-    exit()
-time.sleep(2.22)
-html = driver.find_element(By.TAG_NAME, "body")
-# driver.find_element(By.CLASS_NAME, 'css-175oi2r').click()
+MAIN_CLASS_NAME = 'css-175oi2r'
+QUESTION_CLASS_NAME = 'css-1rynq56 r-1wns2tv r-ubezar r-fdjqy7 r-9cokr0 r-1xnzce8 r-35wr9i r-13uqrnb r-1it3c9n'
+ASKER_CLASS_NAME = 'css-1rynq56 r-fdjqy7 r-9cokr0 r-1xnzce8 r-9krj61 r-1b43r93 r-5x3879 r-13uqrnb r-1it3c9n'
+DATE_CLASS_NAME = 'css-1rynq56 r-fdjqy7 r-9cokr0 r-1xnzce8 r-1put3z6 r-1b43r93 r-1bymd8e r-5x3879 r-13uqrnb r-1it3c9n'
+ANSWER_CLASS_NAME = 'css-1rynq56 r-1wns2tv r-ubezar r-fdjqy7 r-9cokr0 r-1xnzce8 r-5x3879 r-13uqrnb r-1it3c9n'
+# URL = 'https://curiouscat.live/Wasp29'
+# URL = 'https://curiouscat.live/mohfarouk94'
+URL = 'https://curiouscat.live/discobanana'
 
 
-# Scroll down to load more content if necessary (add a scroll loop if needed)
-SCROLL_PAUSE_TIME = 0.1
-COUNTER = 0
-all_divs = []
+def start_driver(waiting_time=2.22, url=URL):
+    chrome_options = Options()
+    # chrome_options.add_argument("--disable-blink-features=AutomationControlled")
+    chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    chrome_options.add_experimental_option('useAutomationExtension', False)
+    chrome_options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36")
+    # chrome_options.add_argument('--headless')
+    driver = webdriver.Chrome(chrome_options)
+    driver.get(URL)
 
-while COUNTER < 500:
-    COUNTER += 1
-    print(f'Scolling ({COUNTER})')
-    soup = BeautifulSoup(driver.page_source, "html.parser")
-    divs = soup.find_all("div", class_="css-175oi2r")
-    all_divs.extend(divs)
-    # print(len(html.text))
-    # Scroll down to the bottom
-    html.click()
-    # time.sleep(1.111)
-    html.send_keys(Keys.DOWN)
-    # action.send_keys_to_element(html, "END").perform()
-    # driver.execute_script("window.scrollTo(0, window.scrollY+500);")
-
-    # Wait for new content to load
-    time.sleep(SCROLL_PAUSE_TIME)
-
-    # Calculate new scroll height and compare with last scroll height
-    # new_height = driver.execute_script("return document.body.scrollHeight")
-    # print(new_height)
-
-
-# Get the page source and parse it using BeautifulSoup
-# soup = BeautifulSoup(driver.page_source, "html.parser")
-
-
-# Find all the main divs with class "css-175oi2r"
-# divs = soup.find_all("div", class_="css-175oi2r")
-
-# List to hold the dictionaries
-data_list = []
-
-# Iterate through each main div to extract sub divs
-for div in all_divs:
     try:
-        # Extract the question
-        question_div = div.find("div", class_="css-1rynq56 r-1wns2tv r-ubezar r-fdjqy7 r-9cokr0 r-1xnzce8 r-35wr9i r-13uqrnb r-1it3c9n")
-        question = question_div.get_text(strip=True) if question_div else "N/A"
+        WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.CLASS_NAME, MAIN_CLASS_NAME)))
+    except TimeoutException:
+        print("Loading took too much time!")
+        driver.quit()
+        exit()
+    time.sleep(waiting_time)
+    html = driver.find_element(By.TAG_NAME, "body")
+    return driver, html
 
-        # Extract the asker
-        asker_div = div.find("div", class_="css-1rynq56 r-fdjqy7 r-9cokr0 r-1xnzce8 r-9krj61 r-1b43r93 r-5x3879 r-13uqrnb r-1it3c9n")
-        asker = asker_div.get_text(strip=True) if asker_div else "N/A"
 
-        # Extract the date
-        date_div = div.find("div", class_="css-1rynq56 r-fdjqy7 r-9cokr0 r-1xnzce8 r-1put3z6 r-1b43r93 r-1bymd8e r-5x3879 r-13uqrnb r-1it3c9n")
-        date = date_div.get_text(strip=True) if date_div else "N/A"
+def scrape(driver, html, scroll_pause_time=3, n_scrolls=25):
+    all_divs = []
+    prev_height = len(html.text)
+    html.click()
 
-        # Extract the answer
-        answer_div = div.find("div", class_="css-1rynq56 r-1wns2tv r-ubezar r-fdjqy7 r-9cokr0 r-1xnzce8 r-5x3879 r-13uqrnb r-1it3c9n")
-        answer = answer_div.get_text(strip=True) if answer_div else "N/A"
+    for _ in tqdm(range(n_scrolls)):
+        soup = BeautifulSoup(driver.page_source, "html.parser")
+        divs = soup.find_all("div", class_=MAIN_CLASS_NAME)
+        all_divs.extend(divs)
+        # html.click()
+        html.send_keys(Keys.END)
+        time.sleep(scroll_pause_time)
 
-        # Create a dictionary for this entry
-        data = {
-            "question": question,
-            "asker": asker,
-            "date": date,
-            "answer": answer
-        }
+        try:
+            new_height = len(html.text)
+            if new_height == prev_height:
+                print('Early Stop! I touched the bottom!')
+                break
+            prev_height = new_height
+        except Exception:
+            pass
 
-        # Append to the list
-        if 'N/A' not in data.values() and data not in data_list:
-            data_list.append(data)
+    return all_divs
 
-    except Exception as e:
-        print(f"Error processing div: {e}")
-        continue
 
-# Close the browser after scraping
-driver.quit()
+def end_driver(driver):
+    driver.quit()
+
+
+def extract_data(all_divs):
+    data_list = []
+    for div in tqdm(all_divs):
+        try:
+            question_div = div.find("div", class_=QUESTION_CLASS_NAME)
+            question = question_div.get_text(strip=True) if question_div else "N/A"
+
+            asker_div = div.find("div", class_=ASKER_CLASS_NAME)
+            asker = asker_div.get_text(strip=True) if asker_div else "N/A"
+
+            date_div = div.find("div", class_=DATE_CLASS_NAME)
+            date = date_div.get_text(strip=True) if date_div else "N/A"
+
+            answer_div = div.find("div", class_=ANSWER_CLASS_NAME)
+            answer = answer_div.get_text(strip=True) if answer_div else "N/A"
+
+            data = {
+                "question": question,
+                "asker": asker,
+                "date": date,
+                "answer": answer
+            }
+
+            if 'N/A' not in data.values() and data not in data_list:
+                data_list.append(data)
+
+        except Exception as e:
+            print(f"Error processing div: {e}")
+            continue
+    return data_list
+
+
+driver, html = start_driver()
+all_divs = scrape(driver, html)
+end_driver(driver)
+data_list = extract_data(all_divs)
 
 print(len(data_list))
 print(data_list[-5:])
 df = pd.DataFrame(data_list)
-df.to_csv('cat.csv', index=False)
+df.to_csv(f'cat{int(time.time())}.csv', index=False)
